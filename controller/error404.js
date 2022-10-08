5ea0bfe906f1d929d25646c61a89f73c3d8fddb0
Mini Lemon <44203733+tothanhquy@users.noreply.github.com>
2022-10-08 21:43:58 +0700

    finish 2

--- controller/error404.js
+++ controller/error404.js
@@ -2,5 +2,6 @@ var GenaralMethod = require("../GenaralMethod");
 
 module.exports = function(req, res, next) {
     let resOj = GenaralMethod.getResRouterObject();
+    resOj.error = "404 page not exist";
     res.send(resOj);
 }
