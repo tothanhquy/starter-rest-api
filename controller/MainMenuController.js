6c3e669e98158b3f0631fc1d910df833ef5e15c6
Mini Lemon <44203733+tothanhquy@users.noreply.github.com>
2022-10-09 22:21:12 +0700

    next

--- controller/MainMenuController.js
+++ controller/MainMenuController.js
@@ -9,6 +9,7 @@ exports.loadDataCharts = function(req, res, next) {
         let rememberUserName = req.query.rememberUserName;
         let rememberAccessToken = req.query.rememberUserName;
         if (Controller.checkRemember(rememberUserName, rememberAccessToken)) {
+
             let level3 = db.getChartByLevel(3);
             resFunc.data.level3x3 = level3.data;
             let level4 = db.getChartByLevel(4);
@@ -17,6 +18,15 @@ exports.loadDataCharts = function(req, res, next) {
             resFunc.data.level5x5 = level5.data;
             let level6 = db.getChartByLevel(6);
             resFunc.data.level6x6 = level6.data;
+
+            let timePlay = db.getTimePlayByUser(rememberUserName);
+
+            resFunc.playPause = false;
+            if (timePlay.code == 1) {
+                if (timePlay.timeStart != 0) {
+                    resFunc.playPause = true;
+                }
+            }
             resFunc.code = 1;
         } else {
             resFunc.error = "not_login";
