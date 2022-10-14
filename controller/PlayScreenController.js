aeae0d088d0c0d589d2011421fcea2ef490b032d
Mini Lemon <44203733+tothanhquy@users.noreply.github.com>
2022-10-14 09:30:06 +0700

    s

--- controller/PlayScreenController.js
+++ controller/PlayScreenController.js
@@ -50,9 +50,9 @@ exports.createGamePlay = async function(req, res, next) {
             //let rememberAccount = await Controller.checkRemember(rememberUserName, rememberAccessToken);
             if (await Controller.checkRemember(rememberUserName, rememberAccessToken)) {
                 let imageName = getRandomImageName();
-                let arrayIndex = getRandomArrayIndex(level * level);
                 res.send(JSON.stringify("imageName"));
                 return;
+                let arrayIndex = getRandomArrayIndex(level * level);
                 let timeStart = Date.now();
                 let timePauseDefault = 0;
                 let timeMinusDefault = 0;
