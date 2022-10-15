524b895073adb26c34413a53991cb1e7658e4768
Mini Lemon <44203733+tothanhquy@users.noreply.github.com>
2022-10-15 13:37:58 +0700

    s

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,7 +1,6 @@
 var GenaralMethod = require("../GenaralMethod");
 var AccountModel = require("../model/AccountModel");
 var bcrypt = require("bcryptjs");
-const { json } = require("express");
 
 exports.checkRemember = function(userName, accessToken) {
     return new Promise(async(resolve, reject) => {
@@ -17,29 +16,9 @@ exports.checkRemember = function(userName, accessToken) {
 }
 
 exports.hashPassword = function(pass) {
-    // let res = await new Promise((resolve, reject) => {
-    //     bcrypt.genSalt(10, (err, salt) => {
-    //         bcrypt.hash(pass, salt, function(err, hash) {
-    //             if (err) return resolve("error");
-    //             return resolve(hash);
-    //         });
-    //     })
-    // });
-    // // let salt = await bcrypt.genSalt(10);
-    // // let res = await bcrypt.hash(pass, salt);
-    // return res;
     return bcrypt.hashSync(pass, 10);
 }
 exports.verifyPassword = function(pass, hash) {
-    // let res = await new Promise(async(resolve, reject) => {
-    //     bcrypt.compare(pass, hash, function(err, result) {
-    //         if (result) {
-    //             return resolve(true);
-    //         }
-    //         return resolve(false);
-    //     });
-    // });
-    // return res;
     return bcrypt.compareSync(pass, hash);
 }
 exports.getParams = function(req) {
--- data/sql.sql
+++ data/sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: sql6.freemysqlhosting.net
--- Thời gian đã tạo: Th10 14, 2022 lúc 04:14 AM
+-- Thời gian đã tạo: Th10 15, 2022 lúc 06:34 AM
 -- Phiên bản máy phục vụ: 5.5.62-0ubuntu0.14.04.1
 -- Phiên bản PHP: 7.0.33-0ubuntu0.16.04.16
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `sql6524898`
+-- Cơ sở dữ liệu: `sql6526407`
 --
 
 -- --------------------------------------------------------
@@ -32,16 +32,16 @@ CREATE TABLE `account` (
   `id` int(11) NOT NULL,
   `user_name` varchar(20) COLLATE utf16_unicode_ci NOT NULL,
   `pass_word` varchar(300) COLLATE utf16_unicode_ci NOT NULL,
-  `level3x3` int(11) DEFAULT NULL,
-  `level4x4` int(11) DEFAULT NULL,
-  `level5x5` int(11) DEFAULT NULL,
-  `level6x6` int(11) DEFAULT NULL,
+  `level3x3` bigint(11) DEFAULT NULL,
+  `level4x4` bigint(11) DEFAULT NULL,
+  `level5x5` bigint(11) DEFAULT NULL,
+  `level6x6` bigint(11) DEFAULT NULL,
   `access_token` varchar(300) COLLATE utf16_unicode_ci DEFAULT NULL,
   `play_matrix` varchar(2000) COLLATE utf16_unicode_ci NOT NULL DEFAULT '[]',
   `image_name` varchar(100) COLLATE utf16_unicode_ci DEFAULT NULL,
-  `time_start` int(11) NOT NULL DEFAULT '0',
-  `time_pause` int(11) NOT NULL DEFAULT '0',
-  `time_minus` int(11) NOT NULL DEFAULT '0'
+  `time_start` bigint(11) NOT NULL DEFAULT '0',
+  `time_pause` bigint(11) NOT NULL DEFAULT '0',
+  `time_minus` bigint(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 --
@@ -49,7 +49,7 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id`, `user_name`, `pass_word`, `level3x3`, `level4x4`, `level5x5`, `level6x6`, `access_token`, `play_matrix`, `image_name`, `time_start`, `time_pause`, `time_minus`) VALUES
-(6, 'as', '$2a$10$m4Z23IsaZTNfUHCQbG8UpucdoGp.2AQuaOhW3aBENDRKREIvfBVuO', NULL, NULL, NULL, NULL, '5NQpsT2bxY7ehif9sQLd1rO0NchJyU63iA1cJtBvBcZU1wD4LL', '[2,1,7,4,6,3,0,5,8]', 'pexels-lisa-fotios-2362875.jpg', 2147483647, 0, 0);
+(6, 'as', '$2a$10$m4Z23IsaZTNfUHCQbG8UpucdoGp.2AQuaOhW3aBENDRKREIvfBVuO', 247, NULL, NULL, NULL, 'cjhBd64P1R6RFC9r389VwfHlGaKFCK6deOJLTJpFzGYmbzOQoC', '[0,1,2,3,4,5,6,7,8,13,11,14,12,9,10,15]', 'pexels-lisa-fotios-2362875.jpg', 2147483647, 0, 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
